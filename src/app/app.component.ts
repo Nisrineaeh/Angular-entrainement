0a33d137d7d55daea27d9db4e8b1d30a72c9cd52
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-07-02 17:49:39 +0200

    + class Pokemon & initialisation dans son mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{  pokemonList[0] }} !</h1>`
+  template: `<h1>Liste de Pokémons</h1>`
 })
 export class AppComponent implements OnInit {
-  pokemonList = ['Bulbizarre','Salamèche','Carapuce'];
+  pokemonList = POKEMONS;
 
   ngOnInit(){
     console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[11]);
   }
-}
+
+  selectPokemon(pokemon: Pokemon){
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);// toujour nom mais param pokemonListMock
+  }
+} 
