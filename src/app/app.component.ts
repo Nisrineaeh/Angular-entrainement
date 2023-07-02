49319dfbd0243c1a06ab18f240a3ac3be0eafd38
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-07-02 16:48:03 +0200

    rename page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{  pokemonList[1] }} !</h1>  <h2>Pokémons</h2>`
+  template: `<h1>Welcome to {{  pokemonList[0] }} !</h1>`
 })
 export class AppComponent implements OnInit {
   pokemonList = ['Bulbizarre','Salamèche','Carapuce'];
